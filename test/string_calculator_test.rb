79ff2cd2c4bab6cdd4e00da8161cc0e8a9eb2340
jayeshpatil01 <jayeshpatil8041@gmail.com>
2024-07-09 17:52:59 +0530

    Add test for single number

--- test/string_calculator_test.rb
+++ test/string_calculator_test.rb
@@ -6,4 +6,9 @@ class StringCalculatorTest < Minitest::Test
     calculator = StringCalculator.new
     assert_equal 0, calculator.add("")
   end
+
+  def test_add_single_number
+    calculator = StringCalculator.new
+    assert_equal 1, calculator.add("1")
+  end
 end
